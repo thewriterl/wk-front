d7cd3e62d37a32be8b3b16daede94837eba49e17
Luiz Fernando França <luiz.darthvader@gmail.com>
2021-02-24 11:23:34 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+import {MatCardModule} from '@angular/material/card';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
